1ffb721c2d5dda6c7ff33a0ab0b386ad2ef3d5a4
Danielbano1 <146651597+Danielbano1@users.noreply.github.com>
2024-09-14 16:04:35 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include"converteutf832.h"
+
+
+int main() {
+	int erro;
+	// 8p32
+	FILE* arquivo_entrada = fopen("texto.txt", "rb");
+	FILE* arquivo_saida = fopen("8p32.txt", "wb");
+
+	erro = convUtf8p32(arquivo_entrada, arquivo_saida);
+	printf("%d\n", erro);
+
+	fclose(arquivo_entrada);
+	fclose(arquivo_saida);
+
+	// 32p8
+	FILE* arquivo_entrada2 = fopen("8p32.txt", "rb");
+	FILE* arquivo_saida2 = fopen("32p8.txt", "wb");
+
+	erro = convUtf8p32(arquivo_entrada, arquivo_saida);
+	printf("%d\n", erro);
+
+	fclose(arquivo_entrada);
+	fclose(arquivo_saida);
+
+	return 0;
+}
